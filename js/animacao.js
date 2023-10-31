c0809a7cc3cb7c6ebeb4f8b5afb771357c71dff6
Arthur Moura <110835670+Arthur-Moura-Cardoso@users.noreply.github.com>
2023-10-31 05:45:19 -0300

    Add files via upload

---
+++ js/animacao.js
@@ -0,0 +1,39 @@
+const linhas = document.querySelectorAll("hr")
+const icone = document.querySelectorAll(".icons")
+const titulos = document.querySelectorAll(".titulo-text")
+
+const myObserver = new IntersectionObserver ((event) => {
+    event.forEach((entry) => {
+        
+        if(entry.isIntersecting &&
+            entry.target.classList == "linha") {
+                entry.target.style.width = "90%"
+            }
+        
+        if(entry.isIntersecting &&
+            entry.target.classList != "linha" &&
+            entry.target.classList != "icons") {
+            entry.target.classList.add('escrita')
+        }
+
+        if(entry.isIntersecting &&
+            entry.target.classList == "icons" &&
+            entry.target.trigger === "") {
+                entry.target.trigger = "in"
+        }
+    })
+}, {
+    threshold: 1
+})
+
+linhas.forEach(hr => {
+    myObserver.observe(hr)
+})
+
+icone.forEach(icon => {
+    myObserver.observe(icon)
+})
+
+titulos.forEach(titulo => {
+    myObserver.observe(titulo)
+})
